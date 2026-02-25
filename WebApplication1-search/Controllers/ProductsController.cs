fa023991bec50c3f6159abff0b59a6a82bb67f21
ispan_grapefarm <toshirayu@gmail.com>
2026-02-25 15:37:47 +0800

    商品index頁面

---
+++ WebApplication1-search/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1_search.Models.EfModels;
+using WebApplication1_search.ViewModels;
+
+namespace WebApplication1_search.Controllers
+{
+	public class ProductsController : Controller
+	{
+		private readonly ISpanDemoContext _context;
+
+		public ProductsController(ISpanDemoContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			
+			//取得紀錄
+			var data = _context.Products
+				.AsNoTracking()  //不追蹤變更，提升效能
+				.Include(x => x.Category) //inner join，一次撈出產品與分類資料
+				.OrderBy(x => x.Category.DisplayOrder) //先依照分類排序
+				.ThenBy(x => x.UnitPrice) // 再依照售價排序
+				.Select(x => new ProductIndexItemViewModel
+				{
+					Id = x.Id,
+					CategoryName = x.Category.CategoryName,
+					ProductName = x.ProductName,
+					UnitPrice = x.UnitPrice
+				})
+				.ToList();  //LINQ的延遲執行在此時才會真正執行SQL查詢
+
+			return View(data);
+		}
+	}
+}
---
+++ WebApplication1-search/ViewModels/ProductIndexItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1_search.ViewModels
+{
+	public class ProductIndexItemViewModel
+	{
+		public int Id { get; set; }
+
+		[Display(Name = "分類名稱")]
+		public string CategoryName { get; set; }
+
+		[Display(Name = "商品名稱")]
+		public string ProductName { get; set; }
+
+		[Display(Name = "售價")]
+		public decimal UnitPrice { get; set; }
+	}
+}
