796042a2164d31642ed08adacff59bf859ec96f3
ispan_grapefarm <toshirayu@gmail.com>
2026-02-25 10:52:09 +0800

    加入專案檔案。

---
+++ WebApplication3/Controllers/UploadFile01Controller.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+	public class UploadFile01Controller : Controller
+	{
+		private readonly IWebHostEnvironment _env;
+		private readonly string _uploads;
+
+		public UploadFile01Controller(IWebHostEnvironment env)
+		{
+			_env = env;
+			//找到本專案的根目錄，計算出要存放的路徑
+			_uploads = Path.Combine(_env.WebRootPath, "uploads");
+		}
+
+		public IActionResult Create(IFormFile f)
+		{
+			
+			var allowExts = new string[] { ".jpg", ".jpeg", ".png" };
+			//判斷沒有上傳f
+			if (f==null || f.Length == 0)
+			{
+				ModelState.AddModelError("", "必須上傳檔案");
+			}
+			else
+			{
+				//判斷副檔名，檔名及大小，......是否符合要求
+				string ext= Path.GetExtension(f.FileName); //".jpg"
+				if (allowExts.Contains(ext) == false)
+				{
+					ModelState.AddModelError("", "副檔名不符合規定");
+				}
+				else
+				{
+					//為了避免檔名重複，必須取一個唯一的檔名，但副檔名相同
+					//ToString()會生成一個有"-"的字串
+					//ToString("N")會生成一個沒有"-"的字串，較短，較不易超出windows的檔名長度限制
+					string newFileName = Guid.NewGuid().ToString("N") + ext;
+
+					//存檔
+
+					//新增紀錄
+				}
+			}
+
+				return View();
+		}
+	}
+}
---
+++ WebApplication3/Models/Info.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication3.Models
+{
+	public class Info
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+		//field吃不到model binding的效果
+		public string Address;
+	}
+}
