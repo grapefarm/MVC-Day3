a78a63dacd8b7a6fcf27ad629b8b19ea5ee5df47
ispan_grapefarm <toshirayu@gmail.com>
2026-02-25 16:11:30 +0800

    新增篩選條件，重點:IQueryable IEnumerable

--- WebApplication1-search/Controllers/ProductsController.cs
+++ WebApplication1-search/Controllers/ProductsController.cs
@@ -18,13 +18,27 @@ namespace WebApplication1_search.Controllers
 		//從URL Query String接收搜尋條件 ?productName=xxx&priceStart=100&priceEnd=500
 		public IActionResult Index(ProductCriteria criteria)
 		{
-			
 			//取得紀錄
-			var data = _context.Products
+			IQueryable<Product> query = _context.Products
 				.AsNoTracking()  //不追蹤變更，提升效能
 				.Include(x => x.Category) //inner join，一次撈出產品與分類資料
 				.OrderBy(x => x.Category.DisplayOrder) //先依照分類排序
-				.ThenBy(x => x.UnitPrice) // 再依照售價排序
+				.ThenBy(x => x.UnitPrice); // 再依照售價排序
+
+			if (criteria.PriceStart.HasValue)
+			{
+				query = query.Where(x => x.UnitPrice >= criteria.PriceStart.Value);
+			}	
+			if (criteria.PriceEnd.HasValue)
+			{
+				query = query.Where(x => x.UnitPrice <= criteria.PriceEnd.Value);
+			}
+			if (string.IsNullOrEmpty(criteria.ProductName) == false)
+			{
+				query = query.Where(x => x.ProductName.Contains(criteria.ProductName));
+			}
+
+			IEnumerable<ProductIndexItemViewModel> data = query
 				.Select(x => new ProductIndexItemViewModel
 				{
 					Id = x.Id,
